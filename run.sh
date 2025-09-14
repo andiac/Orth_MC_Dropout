b0cd1afcc2070dde88c207b8c5f4df566879230d
Andi Zhang <me@andi.ac>
2025-09-14 15:45:36 +0000

    update

--- run.sh
+++ run.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 CASE_NAMES=(
+    "kimoju_cloud"
+    "cat2_monster_toy"
     "duck_toy_robot_toy"
 )
 
 for CASE_NAME in ${CASE_NAMES[@]}; do
-    python normal_merge.py --config ./configs/${CASE_NAME}.toml
+    python direct_merge.py --config ./configs/${CASE_NAME}.toml
     python dropout_merge.py --config ./configs/${CASE_NAME}.toml
     python dropout_merge.py --config ./configs/${CASE_NAME}.toml --orthogonal
     python concatenate.py --case_name ${CASE_NAME}
